17d92a749d8432fac0ae7c02e8337df40e435639
kushal-raj-dangol <kushaldangol25@gmail.com>
2025-08-22 21:34:23 +0545

    Assignment 1

--- Assignment 1/Assignment-1(2).py
+++ Assignment 1/Assignment-1(2).py
@@ -8,6 +8,6 @@ country = input("Which country are you from?")
 print("Hello "+name.upper())
 print("Your age is", age)
 print(f"Your height is {height :.2f} feet")
-print("You are from", country)
+print("You are from", country.title())
 print(f"Your nickname is {name.upper()[0:2]}"+f"{name.upper()[-2:]}")
 
